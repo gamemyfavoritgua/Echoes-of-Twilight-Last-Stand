eb240a353cb571f3198b29ede3d8eb6982d5da7b
rhaken <rhakenss@gmail.com>
2025-05-14 09:05:37 +0700

    rune kasar

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,28 @@
 class_name Player extends CharacterBody2D
 
 @export var speed := 200.0
+@export var max_health := 100
+@export var health := 50
+@export var stamina := 50
+@export var is_alive := true
+
 @onready var animation_player = $AnimatedSprite2D
+
+func _ready():
+	add_to_group("player")
+
+func _process(delta: float) -> void:
+	print("nyawa", health)
+
+func take_damage(amount: int):
+	health -= amount
+	if health <= 0:
+		die()
+		
+func heal(amount: int):
+	health = min(health + amount, max_health)
+	
+func die():
+	is_alive = false
+	animation_player.play("death")
+	set_physics_process(false)
---
+++ scripts/rune_health.gd
@@ -0,0 +1,39 @@
+extends Node2D
+@export var type := "health"
+@export var amount := 20
+@export var float_height := 5.0
+@export var float_speed := 2.0
+
+var initial_position: Vector2
+var tween: Tween
+
+func _ready():
+	initial_position = position
+	create_floating_effect()
+
+func create_floating_effect():
+	tween = create_tween()
+	tween.set_loops()
+	tween.tween_property(self, "position",initial_position + Vector2(0, -float_height),float_speed / 2.0)
+	tween.tween_property(self, "position",initial_position + Vector2(0, float_height),float_speed / 2.0)
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Body entered: ", body.name)
+	
+	if body.is_in_group("player"):
+		print("Player detected!")
+		apply_effect(body)
+		if tween:
+			tween.kill()
+		queue_free()
+
+func apply_effect(player):
+	match type:
+		"health":
+			if player.has_method("heal"):
+				player.heal(amount)
+				print("Healing player for ", amount, " health")
+		"mana":
+			if player.has_method("add_mana"):
+				player.add_mana(amount)
+				print("Adding ", amount, " mana to player")
