ce614fdc3f1443006b0359dedcf7e1a64ae57f3b
rhaken <rhakenss@gmail.com>
2025-05-22 04:38:03 +0700

    integrate all sound

--- scripts/enemy/states/hurt.gd
+++ scripts/enemy/states/hurt.gd
@@ -1,13 +1,23 @@
 extends GoblinState
 
+var hurt_stream := preload("res://assets/Sound Asset/sfx/hit.mp3")
+
 ## Called by the state machine upon changing the active state. The `data` parameter
 ## is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(previous_state_path: String, data := {}) -> void:
-    player.animation_player.play("hurt")
-    
+	player.animation_player.play("hurt")
+	
+	var hurt_sfx = AudioStreamPlayer.new()
+	hurt_sfx.stream = hurt_stream
+	player.add_child(hurt_sfx)
+	hurt_sfx.play()
+	
+	await get_tree().create_timer(2.0).timeout
+	hurt_sfx.queue_free()
+	
 ## Called by the state machine on the engine's physics update tick.
 func physics_update(_delta: float) -> void:
-    if player.animation_player.is_playing():
-        return
-        
-    finished.emit(WALK)
+	if player.animation_player.is_playing():
+		return
+		
+	finished.emit(WALK)
--- scripts/states/hurt.gd
+++ scripts/states/hurt.gd
@@ -1,8 +1,18 @@
 extends PlayerState
 
+var hurt_stream := preload("res://assets/Sound Asset/sfx/hit.mp3")
+
 func enter(previous_state_path: String, data := {}) -> void:
 	player.velocity = Vector2.ZERO
 	player.animation_player.play("hurt")
+	
+	var hurt_sfx = AudioStreamPlayer.new()
+	hurt_sfx.stream = hurt_stream
+	player.add_child(hurt_sfx)
+	hurt_sfx.play()
+	
+	await get_tree().create_timer(2.0).timeout
+	hurt_sfx.queue_free()
 
 func physics_update(_delta: float) -> void:
 	# Wait for animation to complete before returning to IDLE
