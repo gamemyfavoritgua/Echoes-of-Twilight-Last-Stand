71ac9f29a909d935e52bdcf2b0e5ca71b520d39a
riapis <fari.hafizh@ui.ac.id>
2025-05-22 03:33:34 +0700

    HUD done

--- scripts/twilight.gd
+++ scripts/twilight.gd
@@ -1,10 +1,10 @@
 extends Node
 
-@export var fill_duration_sec: float = 60.0  # total detik sampai penuh
-@export var buff_amount: int = 50
+@export var fill_duration_sec: float = 10.0  # total detik sampai penuh
+@export var buff_amount: int = 20
 @export var health_drain: int = 5
 
-@onready var twilight_progress = $TwilightProgress
+@onready var twilight_progress = $CanvasLayer/TwilightProgress
 @onready var fill_timer = $FillTimer
 @onready var drain_timer = $DrainTimer
 
@@ -14,6 +14,7 @@ var player: CharacterBody2D = null
 func _ready():
 	fill_timer.start()
 	drain_timer.stop()
+	twilight_progress.max_value = fill_duration_sec
 	
 func _on_fill_timer_timeout():
 	if buff_applied:
