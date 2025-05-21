e32b9648befeadd7a877aee922578367c5d12668
rhaken <rhakenss@gmail.com>
2025-05-21 17:42:15 +0700

    rune system done

--- scripts/enemy/goblin.gd
+++ scripts/enemy/goblin.gd
@@ -3,19 +3,38 @@ class_name Goblin extends CharacterBody2D
 @export var speed := 50.0
 @export var max_health := 100.0
 var health := max_health
+@export_file("*.tscn") var item_scene_path := ""
+@export var drop_chance := 0.2
 
 @onready var animation_player = $AnimatedSprite2D
 
 func take_damage(damage_amount: float) -> void:
 	health -= damage_amount
+	var drop_position = global_position
 	
 	if health <= 0:
-		# If health depleted, go to death state
+		print(drop_position, "hehehehe")
 		var state_machine = get_node("StateMachine")
 		if state_machine:
 			state_machine._transition_to_next_state("Death")
+		_try_drop_item(drop_position)
 	else:
-		# Otherwise go to hurt state
 		var state_machine = get_node("StateMachine")
 		if state_machine:
 			state_machine._transition_to_next_state("Hurt")
+			
+
+func _try_drop_item(pos: Vector2) -> void:
+	if randf() <= drop_chance:
+		_spawn_item(pos)
+		
+func _spawn_item(pos: Vector2) -> void:
+	if item_scene_path.is_empty():
+		return
+	
+	var item_scene = load(item_scene_path)
+	if item_scene:
+		var item_instance = item_scene.instantiate()
+		
+		get_parent().call_deferred("add_child", item_instance)
+		item_instance.global_position = pos
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,7 @@ class_name Player extends CharacterBody2D
 
 @export var speed := 200.0
 @export var health := 100.0
+@export var max_health := 100.0
 
 @onready var twilight = preload("res://scenes/twilight.tscn").instantiate()
 @onready var animation_player = $AnimatedSprite2D
@@ -10,6 +11,7 @@ class_name Player extends CharacterBody2D
 var direction: Vector2 = Vector2(1, 0)
 
 func _ready():
+	add_to_group("player")
 	add_child(twilight)
 	twilight.player = self
 	attack_area.monitoring = false
@@ -26,4 +28,7 @@ func attacked(damage):
 
 func buff(stat):
 	speed += stat
-	health += stat
+	#health += stat
+	
+func heal(amount: int):
+	health = min(health + amount, max_health)
