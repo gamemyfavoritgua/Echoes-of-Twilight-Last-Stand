1a580f530828a06570c7550a6e4915cdb4df7253
riapis <fari.hafizh@ui.ac.id>
2025-05-23 13:06:19 +0700

    apa

--- scripts/states/player_state.gd
+++ scripts/states/player_state.gd
@@ -7,6 +7,6 @@ const ATTACK1 = "Attack1"
 var player: Player
 
 func _ready() -> void:
-    await owner.ready
-    player = owner as Player
-    assert(player != null, "The PlayerState state type must be used only in the player scene. It needs the owner to be a Player node.")
+	await owner.ready
+	player = owner as Player
+	assert(player != null, "The PlayerState state type must be used only in the player scene. It needs the owner to be a Player node.")
